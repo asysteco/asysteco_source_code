4c3e49253d8aa902de0d5753a36404606584b7bd
asysteco <admin@asysteco.com>
2020-08-31 20:10:49 +0200

    index edificios

--- inc/home.php
+++ inc/home.php
@@ -1,28 +1,35 @@
 <?php
 
-if($_SESSION['Perfil'] === 'Admin')
+if(isset($_GET['Numero']))
 {
-  echo '<div class="container-fluid" style="margin-top:50px">';
-    echo "<div class='row'>";
-      echo "<div id='qreader' class='col-xs-12 col-md-4' >";
-          echo "<h3>Fichaje</h3>";
-          include($dirs['inc'] . 'qr-reader.php');
-      echo "</div>";
-      echo "<div class='col-xs-12 col-md-8' style='text-align: center;'>";
-          include($dirs['inc'] . 'filtro-edif-guardias.php');
-          include($dirs['inc'] . 'contenido-guardias.php');
-      echo "</div>";
+    if($_SESSION['Perfil'] === 'Admin')
+    {
+    echo '<div class="container-fluid" style="margin-top:50px">';
+        echo "<div class='row'>";
+        echo "<div id='qreader' class='col-xs-12 col-md-4' >";
+            echo "<h3>Fichaje</h3>";
+            include($dirs['inc'] . 'qr-reader.php');
+        echo "</div>";
+        echo "<div class='col-xs-12 col-md-8' style='text-align: center;'>";
+            include($dirs['inc'] . 'filtro-edif-guardias.php');
+            include($dirs['inc'] . 'contenido-guardias.php');
+        echo "</div>";
+        echo "</div>";
     echo "</div>";
-  echo "</div>";
+    }
+    else
+    {
+    echo '<div class="container-fluid" style="margin-top:50px">';
+        echo "<div class='row'>";
+        echo "<div class='col-xs-12' style='text-align: center;'>";
+            include($dirs['inc'] . 'filtro-edif-guardias.php');
+            include($dirs['inc'] . 'contenido-guardias.php');
+        echo "</div>";
+        echo "</div>";
+    echo "</div>";
+    }
 }
 else
 {
-  echo '<div class="container-fluid" style="margin-top:50px">';
-    echo "<div class='row'>";
-      echo "<div class='col-xs-12' style='text-align: center;'>";
-        include($dirs['inc'] . 'filtro-edif-guardias.php');
-        include($dirs['inc'] . 'contenido-guardias.php');
-      echo "</div>";
-    echo "</div>";
-  echo "</div>";
+    header('Location: index.php');
 }
