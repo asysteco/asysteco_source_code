ad060f541a389b9a4e09d58a5054edf53e138442
asysteco <admin@asysteco.com>
2020-08-31 19:12:56 +0200

    get profesor

--- inc/edit-guardias.php
+++ inc/edit-guardias.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_GET['profesor']))
 {
-    if ($response = $class->query("SELECT MAX($class->profesores.ID) AS Ultimo, MIN($class->profesores.ID) AS Primero, $class->profesores.ID, $class->profesores.Nombre FROM $class->profesores WHERE Activo=1 AND TIPO=2 AND EXISTS (SELECT * FROM $class->horarios WHERE ID_PROFESOR=$class->profesores.ID) ORDER BY ID ASC"))
+    if ($response = $class->query("SELECT MAX($class->profesores.ID) AS Ultimo, MIN($class->profesores.ID) AS Primero, $class->profesores.ID, $class->profesores.Nombre FROM $class->profesores WHERE ID='$_GET[profesor]' AND Activo=1 AND TIPO=2 AND EXISTS (SELECT * FROM $class->horarios WHERE ID_PROFESOR=$class->profesores.ID) ORDER BY ID ASC"))
     {
       if ($response->num_rows > 0)
       {
