410e7c3374450ee3f2944e954432e2e5529a2eac
asysteco <admin@asysteco.com>
2020-08-31 15:19:42 +0200

    pruebas

--- inc/pruebas.php
+++ inc/pruebas.php
@@ -1,12 +1,12 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-xs-12">
-            <div id="btn-response"></div>
+            <div id="guardias-response"></div>
         </div>
     </div>
 </div>
 <script>
 var profesor_act = $('#profesor_act').attr('profesor'); 
 $(document).ready(function(){
-    $(""     )
+    $('#guardias-response').load('index.php?ACTION=horarios&OPT=guardias')
 })
