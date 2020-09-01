8af9b4130dd0dca70fd0c96340b57dcb94605ad7
asysteco <admin@asysteco.com>
2020-09-01 13:24:18 +0200

    pie

--- inc/footer.php
+++ inc/footer.php
@@ -6,6 +6,11 @@
                     </a>
                     <p class="copyright"> Copyright ® Asysteco</p>
                 </div> 
+                <ul>
+                    <li><a href="aviso-legal.html">Aviso legal</a></li>
+                    <li><a href="privacidad.html">Privacidad</a></li>
+                    <li><a href="contactar.html">Contactar</a></li>
+	            </ul>
             </footer>
             <a></a>
     </body>
