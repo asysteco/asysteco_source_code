4668fe16e8ae515265228958f36acca3b6ece77d
asysteco <admin@asysteco.com>
2020-09-01 13:15:52 +0200

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,11 @@
         </div>
+            <footer>
+                <div class="pie">
+                    <a href="#" title="Asysteco" alt="Asysteco" aria-label="Asysteco">
+                        <div class="logo"></div>
+                    </a>
+                    <p class="copyright"> Copyright ® Asysteco</p>
+                </div> 
+            </footer>
     </body>
 </html>
