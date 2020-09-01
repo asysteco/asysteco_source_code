bc0b0ae579cfee1942f3a5bd06275e18091cde6b
asysteco <admin@asysteco.com>
2020-09-01 17:04:14 +0200

    frigo pie

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,26 @@
         </div>
+        <footer>
+<style>            
+.pie {
+    background-color: #222;
+    position: absolute;
+    display: inline-block;
+    bottom: 0;
+    width: 100%;
+    height: 50px;
+    }
+.copyright {
+    text-align: center;
+    color: #9d9d9d;
+}
+</style>
+
+<div class="pie">
+    <a href="#" title="Asysteco" alt="Asysteco" aria-label="Asysteco">
+        <div class="logo"></div>
+    </a>
+    <p class="copyright"> Copyright ® Asysteco</p>
+</div> 
+</footer>
     </body>
 </html>
