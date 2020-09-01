693707d38070d6a2d2bb4e1f798379745f3d7eb5
asysteco <admin@asysteco.com>
2020-09-01 21:50:25 +0200

    update

--- inc/update-guardias.php
+++ inc/update-guardias.php
@@ -8,7 +8,7 @@ $edificio = $_GET['e'];
 if($_GET['SUBOPT'] == 'add')
 {
     $sql = "INSERT INTO $class->horarios (ID_PROFESOR, Dia, HORA_TIPO, Edificio, Aula, Grupo, Hora_entrada, Hora_salida) VALUES ('$profesor', '$dia', '$hora', '$edificio', 'GU". $edificio ."00', 'Guardia', '00:00:00', '00:00:00')";
-    if($response = $class->query($sql))
+    if($class->query($sql))
     {
         include_once($dirs['inc'] . 'actualiza_horas.php');
         include_once($dirs['inc'] . 'marcajes.php');
@@ -21,7 +21,7 @@ if($_GET['SUBOPT'] == 'add')
 elseif($_GET['SUBOPT'] == 'remove')
 {
     $sql = "DELETE FROM $class->horarios WHERE ID_PROFESOR='$profesor' AND Dia='$dia' AND HORA_TIPO='$hora'";
-    if($response = $class->query($sql))
+    if($class->query($sql))
     {
         include_once($dirs['inc'] . 'actualiza_horas.php');
         include_once($dirs['inc'] . 'marcajes.php');
