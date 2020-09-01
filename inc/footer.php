8bf3f0456dad3e5bb41b42f34a5fcdb55fb0ee93
asysteco <admin@asysteco.com>
2020-09-01 18:43:15 +0200

    vuelta a empezar

--- inc/footer.php
+++ inc/footer.php
@@ -1,30 +1,3 @@
         </div>
-            <footer>
-                <style> 
-                body {
-                    flex-wrap: wrap;
-                }           
-                .pie {
-                    background-color: #222;
-                    position: fixed;
-                    display: inline-block;
-                    align-self: flex-end;
-                    bottom: 0;
-                    width: 100%;
-                    height: 50px;
-                    }
-                .copyright {
-                    text-align: center;
-                    color: #9d9d9d;
-                }
-                </style>
-
-                <div class="pie">
-                    <a href="#" title="Asysteco" alt="Asysteco" aria-label="Asysteco">
-                        <div class="logo"></div>
-                    </a>
-                    <p class="copyright"> Copyright ® Asysteco</p>
-                </div> 
-            </footer>
     </body>
 </html>
--- inc/pruebas.php
+++ inc/pruebas.php
@@ -1,23 +1,24 @@
 <footer>
-<style>            
-.pie {
-    background-color: #222;
-    position: absolute;
-    display: inline-block;
-    bottom: 0;
-    width: 100%;
-    height: 50px;
-    }
-.copyright {
-    text-align: center;
-    color: #9d9d9d;
-}
-</style>
+                <style>         
+                .pie {
+                    background-color: #222;
+                    position: fixed;
+                    display: inline-block;
+                    align-self: flex-end;
+                    bottom: 0;
+                    width: 100%;
+                    height: 50px;
+                    }
+                .copyright {
+                    text-align: center;
+                    color: #9d9d9d;
+                }
+                </style>
 
-<div class="pie">
-    <a href="#" title="Asysteco" alt="Asysteco" aria-label="Asysteco">
-        <div class="logo"></div>
-    </a>
-    <p class="copyright"> Copyright ® Asysteco</p>
-</div> 
+                <div class="pie">
+                    <a href="#" title="Asysteco" alt="Asysteco" aria-label="Asysteco">
+                        <div class="logo"></div>
+                    </a>
+                    <p class="copyright"> Copyright ® Asysteco</p>
+                </div> 
 </footer>
