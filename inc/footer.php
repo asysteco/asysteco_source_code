91cd45ae8ad305086e59e9ccb1a2cebf52c7696a
asysteco <admin@asysteco.com>
2020-09-01 18:01:10 +0200

    la luz

--- inc/footer.php
+++ inc/footer.php
@@ -8,6 +8,7 @@
                     bottom: 0;
                     width: 100%;
                     height: 50px;
+                    flex:1;
                     }
                 .copyright {
                     text-align: center;
