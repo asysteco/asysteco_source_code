c938602ee6c74e7a7423183314dad1dc7cec6db3
asysteco <admin@asysteco.com>
2020-09-01 13:47:15 +0200

    pie 8

--- inc/pruebas.php
+++ inc/pruebas.php
@@ -1,20 +1,12 @@
 <footer>
 <style>            
-ul{
-padding-left:0;
-}
-li{
-list-style: none;
-display: inline;
-padding-right: 10px;
-}
 .pie {
-      background-color: #292C44;
+      background-color: black;
       position: absolute;
       display: inline-block;
       bottom: 0;
       width: 100%;
-      height: 40px;
+      height: 60px;
       color: white;
     }
 </style>
@@ -24,11 +16,5 @@ padding-right: 10px;
         <div class="logo"></div>
     </a>
     <p class="copyright"> Copyright ® Asysteco</p>
-            <h4>Generales</h4>
-                <ul>
-                    <li><a href="Aviso-Legal.html">Aviso legal</a></li>
-                    <li><a href="Privacidad.html">Privacidad</a></li>
-                    <li><a href="Contactar.html">Contactar</a></li>
-	            </ul>
 </div> 
 </footer>
