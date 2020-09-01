af9a81e325c13a78a1eb9208045b52095ef12407
asysteco <admin@asysteco.com>
2020-09-01 17:44:08 +0200

    loco loco

--- inc/footer.php
+++ inc/footer.php
@@ -1,32 +1,32 @@
         </div>
-        <footer class="d-flex flex-colum min-vh-100">
-<style>            
-.pie {
-    background-color: #222;
-    position: fixed;
-    display: inline-block;
-    bottom: 0;
-    width: 100%;
-    height: 50px;
-    }
-.copyright {
-    text-align: center;
-    color: #9d9d9d;
-}
-</style>
+            <footer>
+                <style>            
+                .pie {
+                    background-color: #222;
+                    position: absolute;
+                    display: inline-block;
+                    bottom: 0;
+                    width: 100%;
+                    height: 50px;
+                    }
+                .copyright {
+                    text-align: center;
+                    color: #9d9d9d;
+                }
+                </style>
 
-<div class="pie">
-    <a href="#" title="Asysteco" alt="Asysteco" aria-label="Asysteco">
-        <div class="logo"></div>
-    </a>
-    <p class="copyright"> Copyright ® Asysteco</p>
-</div> 
-</footer>
+                <div class="pie">
+                    <a href="#" title="Asysteco" alt="Asysteco" aria-label="Asysteco">
+                        <div class="logo"></div>
+                    </a>
+                    <p class="copyright"> Copyright ® Asysteco</p>
+                </div> 
+            </footer>
     </body>
 </html>
 
 <script>
-    $("button").click(function() {
-        $("section div").toggleClass("pie");
-    });
+$(".pie").click(function() {
+	$(".pie").toggleClass("hidden");
+});
 </script>
