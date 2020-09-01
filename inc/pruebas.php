bf978285a0388cb84c7234b5ba37eb1d4a45d970
asysteco <admin@asysteco.com>
2020-09-01 13:35:15 +0200

    pie

--- inc/pruebas.php
+++ inc/pruebas.php
@@ -1,11 +1,26 @@
+<footer>
 <div class="pie">
     <a href="#" title="Asysteco" alt="Asysteco" aria-label="Asysteco">
         <div class="logo"></div>
     </a>
     <p class="copyright"> Copyright ® Asysteco</p>
 </div> 
+            <nav>
+                <h3>Generales</h3>
                 <ul>
-                    <li><a href="aviso-legal.html">Aviso legal</a></li>
-                    <li><a href="privacidad.html">Privacidad</a></li>
-                    <li><a href="contactar.html">Contactar</a></li>
+                    <li><a href="Aviso-Legal.html">Aviso legal</a></li>
+                    <li><a href="Privacidad.html">Privacidad</a></li>
+                    <li><a href="Contactar.html">Contactar</a></li>
 	            </ul>
+            </nav>
+<style>            
+ul{
+padding-left:0;
+}
+li{
+list-style: none;
+display: inline;
+padding-right: 10px;
+}
+</style>
+</footer>
