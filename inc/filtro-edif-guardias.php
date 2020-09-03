447b595eec615ad6aae7aa26557878488c39dba9
asysteco <admin@asysteco.com>
2020-09-03 18:21:00 +0200

    filtro edificios

--- inc/filtro-edif-guardias.php
+++ inc/filtro-edif-guardias.php
@@ -1,9 +1,3 @@
-
-  <a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=home&OPT=Edificio&Numero=1'; ?>"><button type="button" class="btn btn-success"><span></span> Edificio 1</button></a>
-  <a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=home&OPT=Edificio&Numero=2'; ?>"><button type="button" class="btn btn-success"><span></span> Edificio 2</button></a>
-  <a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=home&OPT=Edificio&Numero=3'; ?>"><button type="button" class="btn btn-success"><span></span> Edificio 3</button></a>
-  <a href="index.php"><button type="button" class="btn btn-success"><span></span> Todos</button></a>
-
 <?php
 echo '<div class="filtro_edificio">';
 if($response = $class->query("SELECT DISTINCT Edificio FROM Horarios WHERE Edificio<>0"))
