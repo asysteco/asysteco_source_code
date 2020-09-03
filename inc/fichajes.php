01cb4a2ac1e53a1a81034403a3fde385f64a6ba5
asysteco <admin@asysteco.com>
2020-09-03 20:01:25 +0200

    fichajes

--- inc/fichajes.php
+++ inc/fichajes.php
@@ -17,38 +17,38 @@ $sql = "SELECT DISTINCT $class->fichar.*
 echo "<h1>Fichajes diarios</h1>";
 if($response = $class->query($sql))
 {
+    echo "</br><table class='table table-striped'>";
+        echo "<thead>";
+            echo "<tr>";
+                echo "<th>Nº</th>";
+                echo "<th>Hora Fichaje</th>";
+                echo "<th>Día semana</th>";
+                echo "<th>Fecha</th>";
+            echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
     if ($response->num_rows > 0)
     {
-        echo "</br><table class='table table-striped'>";
-            echo "<thead>";
-                echo "<tr>";
-                    echo "<th>Nº</th>";
-                    echo "<th>Hora Fichaje</th>";
-                    echo "<th>Día semana</th>";
-                    echo "<th>Fecha</th>";
-                echo "</tr>";
-            echo "</thead>";
-            echo "<tbody>";
-                while ($fila = $response->fetch_assoc())
-                    {
-                        $sep = preg_split('/-/', $fila['Fecha']);
-                        $dia = $sep[2];
-                        $m = $sep[1];
-                        $Y = $sep[0];
-                        echo "<tr>";
-                            echo "<td>$fila[ID]</td>";
-                            echo "<td>$fila[F_entrada]</td>";
-                            echo "<td>$fila[DIA_SEMANA]</td>";
-                            echo "<td>$dia/$m/$Y</td>";
-                        echo "</tr>";
-                    }
-            echo "</tbody>";
-        echo "</table>";
+        while ($fila = $response->fetch_assoc())
+        {
+            $sep = preg_split('/-/', $fila['Fecha']);
+            $dia = $sep[2];
+            $m = $sep[1];
+            $Y = $sep[0];
+            echo "<tr>";
+                echo "<td>$fila[ID]</td>";
+                echo "<td>$fila[F_entrada]</td>";
+                echo "<td>$fila[DIA_SEMANA]</td>";
+                echo "<td>$dia/$m/$Y</td>";
+            echo "</tr>";
+        }
     }
     else
     {
         echo "No existen registros de fichajes.";
     }
+        echo "</tbody>";
+    echo "</table>";
 }
 else
 {
