42f4f919f90b3e4a10ef2c9c05538079b926dd9a
asysteco <admin@asysteco.com>
2020-09-03 18:20:05 +0200

    según edificios

--- inc/filtro-edif-guardias.php
+++ inc/filtro-edif-guardias.php
@@ -1,6 +1,20 @@
-<div class="filtro_edificio">
+
   <a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=home&OPT=Edificio&Numero=1'; ?>"><button type="button" class="btn btn-success"><span></span> Edificio 1</button></a>
   <a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=home&OPT=Edificio&Numero=2'; ?>"><button type="button" class="btn btn-success"><span></span> Edificio 2</button></a>
   <a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=home&OPT=Edificio&Numero=3'; ?>"><button type="button" class="btn btn-success"><span></span> Edificio 3</button></a>
   <a href="index.php"><button type="button" class="btn btn-success"><span></span> Todos</button></a>
-</div>
+
+<?php
+echo '<div class="filtro_edificio">';
+if($response = $class->query("SELECT DISTINCT Edificio FROM Horarios WHERE Edificio<>0"))
+{
+  if($response->num_rows > 0)
+  {
+    while($edificio = $response->fetch_assoc())
+    {
+      echo "<a href='index.php?ACTION=home&OPT=Edificio&Numero=$edificio'; ?>'><button type='button' class='btn btn-success'><span></span> Edificio 1</button></a>";
+    }
+    echo "<a href='index.php'><button type='button' class='btn btn-success'><span></span> Todos</button></a>";
+  }
+}
+echo '</div>';
