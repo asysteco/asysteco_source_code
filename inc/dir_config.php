3da3b85115c17a78c7bfc947d33dbbbc0aab1ce5
asysteco <admin@asysteco.com>
2020-09-03 15:16:19 +0200

    mm romera acepta guiones

--- inc/dir_config.php
+++ inc/dir_config.php
@@ -6,7 +6,7 @@
 $basedir = dirname($_SERVER['DOCUMENT_ROOT']);
 $Instituto = preg_split('/\//', $_SERVER['REQUEST_URI']);
 $subdir = '/' . $Instituto[1];
-preg_match('/^\/[A-Z]+$/i', $subdir) ? $subdir = $subdir : $subdir = '' ;
+preg_match('/^\/[A-Z-]+$/i', $subdir) ? $subdir = $subdir : $subdir = '' ;
 $dirs = [
     'public' => '',
     'inc' => $basedir . $subdir . '/inc/',
