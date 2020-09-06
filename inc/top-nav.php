1ee539a974ef0b74c4a467436eb40cbb3ebe8b50
asysteco <admin@asysteco.com>
2020-09-06 19:04:28 +0200

    Fichaje

--- inc/top-nav.php
+++ inc/top-nav.php
@@ -117,7 +117,7 @@ echo '<nav class="navbar navbar-inverse navbar-fixed-top">';
           echo "</li>";
 
           echo "<li class=' $act_asistencias '>";
-            echo "<a href='$_SERVER[PHP_SELF]?ACTION=fichajes'><span class='glyphicon glyphicon-calendar'></span> Calendario escolar</a>";
+            echo "<a href='$_SERVER[PHP_SELF]?ACTION=fichajes'><span class='glyphicon glyphicon-calendar'></span> Asistencias</a>";
           echo "</li>";
 
           echo "<li class=' $act_guardias '>";
@@ -126,7 +126,7 @@ echo '<nav class="navbar navbar-inverse navbar-fixed-top">';
               <path fill-rule="evenodd" d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1h-3zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5zM.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5z"/>
               <path d="M3 4.5a.5.5 0 0 1 1 0v7a.5.5 0 0 1-1 0v-7zm2 0a.5.5 0 0 1 1 0v7a.5.5 0 0 1-1 0v-7zm2 0a.5.5 0 0 1 1 0v7a.5.5 0 0 1-1 0v-7zm2 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-7zm3 0a.5.5 0 0 1 1 0v7a.5.5 0 0 1-1 0v-7z"/>
               </svg>';
-              echo " Calendario escolar";
+              echo " Interfaz fichaje";
             echo "</a>";
           echo "</li>";
 
