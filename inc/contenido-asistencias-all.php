b6011e78cfe7556714250ef655460976cca8bce7
asysteco <admin@asysteco.com>
2020-09-06 19:36:58 +0200

    grid

--- inc/contenido-asistencias-all.php
+++ inc/contenido-asistencias-all.php
@@ -1,10 +1,10 @@
 <div class="container-fluid" style="margin-top:50px">
     <div class='row' style='text-align: center;'>
-        <div class='col-xs-12 col-md-4'>
+        <div class='col-xs-12 col-md-6'>
 <?php
         include_once($dirs['inc'] . 'fichajes_all.php');
     echo "</div>";
-    echo "<div class='col-xs-12 col-md-8'>";
+    echo "<div class='col-xs-12 col-md-6'>";
         include_once($dirs['inc'] . 'faltas_all.php');
 ?>
         </div>
