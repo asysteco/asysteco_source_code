13d78738c7183dfc3f9749789294a04164e076f8
asysteco <admin@asysteco.com>
2020-09-07 12:07:06 +0200

    reforma

--- inc/form-add-profesor.php
+++ inc/form-add-profesor.php
@@ -3,25 +3,14 @@ echo '<div class="container" style="margin-top:75px">';
     echo "<div class='row'>";
         echo "<div class='col-xs-12'>";
             echo "<h2>Registrar Profesor</h2>";
-            echo "<div>";
             echo "<form action='index.php?ACTION=profesores&OPT=add-profesor' method='post'>";
             echo "<input type='text' name='Iniciales' value='$_POST[Iniciales]' class='form-control' placeholder='Iniciales Profesor'>";
             echo "</br>";
             echo "<input type='text' name='Nombre' value='$_POST[Nombre]' class='form-control' placeholder='Nombre Profesor (Completo)'>";
             echo "</form>";
             echo "</br>";
-            echo "</div>";
-            if($response = $class->query("INSER INTO Profesores VALUES (Iniciales, Nombre)"))
-            {
-                $MSG = "Profesor Añadido Correctamente";
-            }
-            else
-            {
-                $ERR_MSG = $class->ERR_ASYSTECO;
-            }
             echo "<button class='btn btn-info' value='add' name='add-profesor'>Registrar Profesor</button>";
             echo "</form>";
         echo "</div>";
     echo "</div>";
 echo "</div>";
-?>
