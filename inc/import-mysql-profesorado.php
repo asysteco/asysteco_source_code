b371e00e9bed8a0b431bda2f2f4406b19528487d
asysteco <admin@asysteco.com>
2020-09-09 11:09:06 +0200

    Al importar profesorado CHARSET latin1

--- inc/import-mysql-profesorado.php
+++ inc/import-mysql-profesorado.php
@@ -1,5 +1,11 @@
 <?php
 
+// Establecemos UTF8 como cotejamiento de caracteres
+if (!$class->conex->set_charset("latin1")) {
+    printf("Error cargando el conjunto de caracteres utf8: %s\n", $class->conex->error);
+    exit();
+}
+
 if (isset($_POST["import"])) {
     
     $fileName = $_FILES["file"]["tmp_name"];
@@ -35,20 +41,20 @@ if (isset($_POST["import"])) {
                 
                 $iniciales = "";
                 if (isset($column[0])) {
-                    $iniciales = mysqli_real_escape_string($class->conex, utf8_encode($column[0]));
+                    $iniciales = mysqli_real_escape_string($class->conex, $column[0]);
                 }
                 $nombre = "";
                 if (isset($column[1])) {
-                    $nombre = mysqli_real_escape_string($class->conex, utf8_encode($column[1]));
+                    $nombre = mysqli_real_escape_string($class->conex, "$column[1]");
                 }
                 $tutor = "";
                 if (isset($column[2])) {
-                    $tutor = mysqli_real_escape_string($class->conex, utf8_encode($column[2]));
+                    $tutor = mysqli_real_escape_string($class->conex, $column[2]);
                 }
                 $password = $class->encryptPassword($iniciales . '12345');
-                $tipo = mysqli_real_escape_string($class->conex, utf8_encode(2));
-                $activo = mysqli_real_escape_string($class->conex, utf8_encode(1));
-                $sustituido = mysqli_real_escape_string($class->conex, utf8_encode(0));
+                $tipo = mysqli_real_escape_string($class->conex, 2);
+                $activo = mysqli_real_escape_string($class->conex, 1);
+                $sustituido = mysqli_real_escape_string($class->conex, 0);
                 if(! $class->query("INSERT INTO Profesores (Iniciales, Nombre, Password, TIPO, Tutor, Activo, Sustituido)
                 values (
                     '$iniciales',
@@ -60,6 +66,7 @@ if (isset($_POST["import"])) {
                     $sustituido)"))
                 {
                     $ERR_MSG = "<br>Error al importar datos desde CSV.<br>";
+                    $ERR_MSG .= "<br>vuelta $row <br>";
                     $ERR_MSG .= $class->ERR_ASYSTECO;
                 }
                 else
