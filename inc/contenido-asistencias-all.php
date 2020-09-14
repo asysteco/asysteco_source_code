86a09cfa1dc2c4ac9e5750a21a2d4e808abd3a4f
asysteco <admin@asysteco.com>
2020-09-14 19:23:53 +0200

    fluid y espaciado

--- inc/contenido-asistencias-all.php
+++ inc/contenido-asistencias-all.php
@@ -1,10 +1,11 @@
 <div class="container" style="margin-top:50px">
     <div class='row' style='text-align: center;'>
-        <div class='col-xs-12 col-md-6'>
+        <div class='col-xs-12 col-md-5'>
 <?php
         include_once($dirs['inc'] . 'fichajes_all.php');
     echo "</div>";
-    echo "<div class='col-xs-12 col-md-6'>";
+    echo "<div class='col-xs-12 col-md-2'></div>";
+    echo "<div class='col-xs-12 col-md-5'>";
         include_once($dirs['inc'] . 'faltas_all.php');
 ?>
         </div>
