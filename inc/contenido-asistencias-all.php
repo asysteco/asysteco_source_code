cda4f62c9b12a567fb77e7f2dbf1adc2b28999d5
asysteco <admin@asysteco.com>
2020-09-14 19:27:35 +0200

    margeens

--- inc/contenido-asistencias-all.php
+++ inc/contenido-asistencias-all.php
@@ -1,13 +1,15 @@
 <div class="container-fluid" style="margin-top:50px">
     <div class='row' style='text-align: center;'>
-        <div class='col-xs-12 col-md-5'>
+        <div class='col-xs-12 col-md-1'></div>
+        <div class='col-xs-12 col-md-4'>
 <?php
         include_once($dirs['inc'] . 'fichajes_all.php');
     echo "</div>";
     echo "<div class='col-xs-12 col-md-2'></div>";
-    echo "<div class='col-xs-12 col-md-5'>";
+    echo "<div class='col-xs-12 col-md-4'>";
         include_once($dirs['inc'] . 'faltas_all.php');
 ?>
+        <div class='col-xs-12 col-md-1'></div>
         </div>
     </div>
 </div>
