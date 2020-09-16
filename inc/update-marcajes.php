87839a212c5e988f89cc8fbb4b7891a12e417c3b
asysteco <admin@asysteco.com>
2020-09-16 20:38:33 +0200

    Justificada

--- inc/update-marcajes.php
+++ inc/update-marcajes.php
@@ -1,32 +1,76 @@
 <?php
-if($class->query("UPDATE Marcajes SET Asiste=$_GET[Valor] WHERE ID_PROFESOR='$_GET[Profesor]' AND Fecha='$_GET[Fecha]' AND Hora='$_GET[Hora]'"))
+if(isset($_GET['act']) && $_GET['act'] != '')
 {
-    if($_GET['Valor'] == 1)
+    if($_GET['act'] == 'Asiste')
     {
-        $msg = "$_SESSION[Nombre] ha modificado el registro del Día: $_GET[Fecha] Hora: $_GET[Hora] como Asistido.";
+        if($class->query("UPDATE Marcajes SET Asiste=$_GET[Valor] WHERE ID_PROFESOR='$_GET[Profesor]' AND Fecha='$_GET[Fecha]' AND Hora='$_GET[Hora]'"))
+        {
+            if($_GET['Valor'] == 1)
+            {
+                $msg = "$_SESSION[Nombre] ha modificado el registro del Día: $_GET[Fecha] Hora: $_GET[Hora] como Asistido.";
+            }
+            elseif($_GET['Valor'] == 0)
+            {
+                $msg = "$_SESSION[Nombre] ha modificado el registro del Día: $_GET[Fecha] Hora: $_GET[Hora] como Falta.";
+            }
+            elseif($_GET['Valor'] == 2)
+            {
+                $msg = "$_SESSION[Nombre] ha modificado el registro del Día: $_GET[Fecha] Hora: $_GET[Hora] como Actividad Extraescolar.";
+            }
+            else
+            {
+                $msg = "$_SESSION[Nombre] ha enviado un valor incorrecto.";
+            }
+        
+            if(! $class->notificar($_GET['Profesor'], $msg))
+            {
+                echo $class->ERR_ASYSTECO;
+                return false;
+            }
+            return true;
+        }
+        else
+        {
+            echo $class->ERR_ASYSTECO;
+            return false;
+        }
     }
-    elseif($_GET['Valor'] == 0)
+    elseif($_GET['act'] == 'Justificada')
     {
-        $msg = "$_SESSION[Nombre] ha modificado el registro del Día: $_GET[Fecha] Hora: $_GET[Hora] como Falta.";
-    }
-    elseif($_GET['Valor'] == 2)
-    {
-        $msg = "$_SESSION[Nombre] ha modificado el registro del Día: $_GET[Fecha] Hora: $_GET[Hora] como Actividad Extraescolar.";
+        if($class->query("UPDATE Marcajes SET Justificada=$_GET[Valor] WHERE ID_PROFESOR='$_GET[Profesor]' AND Fecha='$_GET[Fecha]' AND Hora='$_GET[Hora]'"))
+        {
+            if($_GET['Valor'] == 1)
+            {
+                $msg = "$_SESSION[Nombre] ha modificado el registro del Día: $_GET[Fecha] Hora: $_GET[Hora] como Falta Justificada.";
+            }
+            elseif($_GET['Valor'] == 0)
+            {
+                $msg = "$_SESSION[Nombre] ha modificado el registro del Día: $_GET[Fecha] Hora: $_GET[Hora] retirando la justificación.";
+            }
+            else
+            {
+                $msg = "$_SESSION[Nombre] ha enviado un valor incorrecto.";
+            }
+        
+            if(! $class->notificar($_GET['Profesor'], $msg))
+            {
+                echo $class->ERR_ASYSTECO;
+                return false;
+            }
+            return true;
+        }
+        else
+        {
+            echo $class->ERR_ASYSTECO;
+            return false;
+        }
     }
     else
     {
-        $msg = "";
+        echo $ERR_MSG = "Acción no válida."; 
     }
-
-    if(! $class->notificar($_GET['Profesor'], $msg))
-    {
-        echo $class->ERR_ASYSTECO;
-        return false;
-    }
-    return true;
 }
 else
 {
-    echo $class->ERR_ASYSTECO;
-    return false;
+    echo $ERR_MSG = "No se ha establecido acción."; 
 }
