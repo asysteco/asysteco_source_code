8ae689a251d2e4a143aa14ca65ec6ea655eb40ea
asysteco <admin@asysteco.com>
2020-09-22 20:05:28 +0200

    favicon while login

--- inc/login_form.php
+++ inc/login_form.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="resources/img/asysteco.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/bootstrap-3.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/login-style.css">
     <script src="js/jquery.min.js"></script>
