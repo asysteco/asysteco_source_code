2d56469aed65fde65a76ea155ed476f6dcbb59c8
asysteco <admin@asysteco.com>
2020-09-25 15:49:37 +0200

    Squashed commit of the following:
    commit 801cef16b63e83d65eacd88151313e99a0b42f53
    Author: asysteco <admin@asysteco.com>
    Date:   Fri Sep 25 15:47:57 2020 +0200
    
        Guarda valor de keys a variables
    
    commit d027b582c4482a475afb4f8d57914841b94f1e0e
    Author: asysteco <admin@asysteco.com>
    Date:   Fri Sep 25 15:40:37 2020 +0200
    
        Conversión de horas a sus respectivas horas reales en Marcajes
    
    commit 54e325a5e1b202646868f9d9e525b2a82cc70d0f
    Author: asysteco <admin@asysteco.com>
    Date:   Fri Sep 25 15:40:10 2020 +0200
    
        deshabilitada interfaz fichaje en menú admin
    
    commit fe57b37abc0b3d6368552c6a959dd9987fe51a04
    Author: asysteco <admin@asysteco.com>
    Date:   Thu Sep 24 21:09:57 2020 +0200
    
        Guardias quita cortina backdrop

--- inc/top-nav.php
+++ inc/top-nav.php
@@ -120,16 +120,6 @@ echo '<nav class="navbar navbar-inverse navbar-fixed-top">';
             echo "<a href='$_SERVER[PHP_SELF]?ACTION=asistencias&OPT=all'><span class='glyphicon glyphicon-calendar'></span> Asistencias actuales</a>";
           echo "</li>";
 
-          echo "<li class=' $act_guardias '>";
-            echo "<a href='$_SERVER[PHP_SELF]?ACTION=guardias'>";
-              echo '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-upc-scan" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1h-3zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5zM.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5z"/>
-              <path d="M3 4.5a.5.5 0 0 1 1 0v7a.5.5 0 0 1-1 0v-7zm2 0a.5.5 0 0 1 1 0v7a.5.5 0 0 1-1 0v-7zm2 0a.5.5 0 0 1 1 0v7a.5.5 0 0 1-1 0v-7zm2 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-7zm3 0a.5.5 0 0 1 1 0v7a.5.5 0 0 1-1 0v-7z"/>
-              </svg>';
-              echo " Interfaz fichaje";
-            echo "</a>";
-          echo "</li>";
-
           echo "<li class=' $act_cal_escolar '>";
             echo "<a href='$_SERVER[PHP_SELF]?ACTION=lectivos'><span class='glyphicon glyphicon-calendar'></span> Calendario escolar</a>";
           echo "</li>";
