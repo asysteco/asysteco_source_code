f6e595ed27ca91dcae630c952abd6216107b5b28
asysteco <admin@asysteco.com>
2020-09-29 19:10:07 +0200

    Refactor en Asysteco class

--- inc/actualiza_profesor.php
+++ inc/actualiza_profesor.php
@@ -1,20 +1,14 @@
 <?php
 
-if($_POST['ID'] != '')
-{
-    $sql = "UPDATE $class->profesores 
-    SET $class->profesores.Iniciales='$_POST[Iniciales]',
-    $class->profesores.Nombre='$_POST[Nombre]', 
-    $class->profesores.Tutor='" . mysqli_real_escape_string($class->conex, $_POST['Tutor']) . "' WHERE $class->profesores.ID=" . mysqli_real_escape_string($class->conex, $_POST['ID']);
-    if($class->query($sql))
-    {
+if ($_POST['ID'] != '') {
+    $sql = "UPDATE Profesores 
+    SET Profesores.Iniciales='$_POST[Iniciales]',
+    Profesores.Nombre='$_POST[Nombre]', 
+    Profesores.Tutor='" . mysqli_real_escape_string($class->conex, $_POST['Tutor']) . "' WHERE Profesores.ID=" . mysqli_real_escape_string($class->conex, $_POST['ID']);
+    if ($class->query($sql)) {
         $MSG = "Datos actualizados correctamente.";
         header("Location:index.php?ACTION=profesores");
-    }
-    else
-    {
+    } else {
         $ERR_MSG = $class->ERR_ASYSTECO;
     }
 }
-
-?>
