a06c30bcf6cd4f6c37b05e0d32b54a8c0a6411dd
Jokerrdl <ruizdeluna15@gmail.com>
2020-10-21 19:30:44 +0200

    fichaje manual + validaciones

--- inc/fichar-manual.php
+++ inc/fichar-manual.php
@@ -4,7 +4,7 @@ echo '<div class="container" style="margin-top:75px">';
         echo "<div class='col-xs-12'>";
             echo "<h2>Fichaje Manual</h2>";
             echo "<form action='index.php?ACTION=fichar-mysql-manual' method='POST'>";
-            if($response = $class->query("SELECT Nombre, ID FROM Profesores ORDER BY Nombre" ))
+            if($response = $class->query("SELECT Nombre, ID FROM Profesores WHERE Tipo <> 1 ORDER BY Nombre" ))
                     {
                 echo "<select id='select_sustituto' name='ID'>";
                     while($fila = $response->fetch_assoc())
@@ -17,11 +17,12 @@ echo '<div class="container" style="margin-top:75px">';
             {
                 $ERR_MSG = $class->ERR_ASYSTECO;
             }
-            echo "<input type='time' name='hora' min='06:00' max='22:00' required>";
-            echo "<br><br>";
-            echo "<input id='fichar-manual' class='form-control' name='dia' type='text' placeholder='' autocomplete='off'>";
-            echo "<br><br>";
-            echo "<button class='btn btn-info pull-right'>Realizar Fichaje</button>";
+                echo "<input type='time' name='hora' min='06:00' max='22:00' required>";
+                echo "<br><br>";
+                echo "<input id='fichar-manual' class='form-control' name='dia' type='text' placeholder='' autocomplete='off'>";
+                echo "<br>";
+                echo "<a href='index.php?ACTION=profesores' class='btn btn-danger pull-left'>Cancelar</a>";
+                echo "<button class='btn btn-info pull-right'>Realizar Fichaje</button>";
             echo "</form>";
         echo "</div>";
     echo "</div>";
--- inc/fichar-mysql-manual.php
+++ inc/fichar-mysql-manual.php
@@ -3,12 +3,37 @@ $dia = explode('/', $_POST['dia']);
 $dia = $dia[2] . '-' . $dia[1] . '-' . $dia[0];
 $diasemana = $class->getDate(strtotime($dia));
 $diasemana = $diasemana['weekday'];
+
 if($class->validFormSQLDate($dia))
 {
-    if($response = $class->query("INSERT INTO fichar (ID_PROFESOR, F_entrada, DIA_SEMANA, Fecha)
-    VALUES ('$_POST[ID]', '$_POST[hora]', '$diasemana', '$dia')"))
+    if($comprobacion = $class->query("SELECT Fecha, Festivo FROM lectivos WHERE Fecha = '$dia' AND Festivo = 'no'"))
     {
-        $MSG = 'Registro insertado correctamente';
+        if($comprobacion->num_rows > 0)
+        {
+            if($res = $class->query("SELECT ID_PROFESOR, Fecha FROM fichar WHERE ID_PROFESOR='$_POST[ID]' AND Fecha='$dia'"))
+            {
+                if($res->num_rows < 1)
+                {
+                    if($response = $class->query("INSERT INTO fichar (ID_PROFESOR, F_entrada, DIA_SEMANA, Fecha)
+                    VALUES ('$_POST[ID]', '$_POST[hora]', '$diasemana', '$dia')"))
+                    {
+                        $MSG = 'Registro insertado correctamente';
+                    }
+                }
+                else
+                {
+                    $ERR_MSG = 'El profesor ya ha fichado hoy.';
+                }
+            }
+            else
+            {
+                $ERR_MSG = $class->ERR_ASYSTECO;
+            }
+        }
+        else
+        {
+            $ERR_MSG = 'No se puede fichar en dias festivos.';
+        }
     }
     else
     {
@@ -18,9 +43,7 @@ if($class->validFormSQLDate($dia))
 else
 {
     $ERR_MSG = 'Error de Fecha';
-    echo "$ERR_MSG";
 }
-
 
 ?>
             
