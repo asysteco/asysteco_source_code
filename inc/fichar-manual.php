a538df15bbc6882b494f1240490fbd142689d49c
asysteco <admin@asystecom.com>
2020-11-09 20:39:55 +0100

    pasar a Ajax fichar manual

--- inc/fichar-manual.php
+++ inc/fichar-manual.php
@@ -7,7 +7,7 @@ echo '<div class="container" style="margin-top:75px">';
             echo "<form action='index.php?ACTION=fichar-mysql-manual' method='POST'>";
                 if($response = $class->query("SELECT Nombre, ID FROM Profesores WHERE Tipo <> 1 ORDER BY Nombre" ))
                 {
-                    echo "<select class='select_profesor' name='ID'>";
+                    echo "<select class='select_profesor' name='ID' id='fichar-manual'>";
                         while($fila = $response->fetch_assoc())
                         {
                             echo "<option value='$fila[ID]'>$fila[Nombre]</option>";
@@ -20,20 +20,22 @@ echo '<div class="container" style="margin-top:75px">';
                 }
                 echo "<br><br>";
                 echo "<label>Hora Fichaje Entrada </label>";
-                echo "<input type='time' name='horaentrada' min='06:00' max='22:00' required style='margin-left: 2%;'>";
+                echo "<input id='add-hora-entrada' type='time' name='horaentrada' min='06:00' max='22:00' required style='margin-left: 2%;'>";
                 echo "<br><br>";
                 
                 if($options['ficharSalida'] == 1)
                 {
                     echo "<label>Hora Fichaje Salida </label>";
-                    echo "<input type='time' name='horasalida' min='06:00' max='22:00' required style='margin-left: 3%;'>";
+                    echo "<input id='add-hora-salida' type='time' name='horasalida' min='06:00' max='22:00' required style='margin-left: 3%;'>";
                     echo "<br><br>";
                 }
-                echo "<input id='fichar-manual' class='form-control' name='dia' type='text' placeholder='Seleccione una fecha' autocomplete='off'>";
+                echo "<input id='add-fecha' class='form-control' name='dia' type='text' placeholder='Seleccione una fecha' autocomplete='off'>";
                 echo "<br>";
                 echo "<a href='index.php?ACTION=profesores' class='btn btn-danger pull-left'>Cancelar</a>";
-                echo "<button class='btn btn-info pull-right'>Realizar Fichaje</button>";
+                echo "<button id='add-manual' action='add' class='btn btn-info pull-right'>Realizar Fichaje</button>";
             echo "</form>";
         echo "</div>";
     echo "</div>";
 echo "</div>";
+
+echo "<script src='js/edit-manual.js'></script>";
--- inc/fichar-mysql-manual.php
+++ inc/fichar-mysql-manual.php
@@ -1,37 +1,37 @@
 <?php
 
-$dia = explode('/', $_POST['dia']);
+$dia = explode('/', $_POST['fecha']);
 $dia = $dia[2] . '-' . $dia[1] . '-' . $dia[0];
 $diasemana = $class->getDate(strtotime($dia));
 $diasemana = $diasemana['weekday'];
+$id = $_POST['ID'];
+$horaentrada = $_POST['horaEntrada'];
+$horasalida = $_POST['horaSalida'];
 
 if ($class->validFormSQLDate($dia)) {
     if ($comprobacion = $class->query("SELECT Fecha, Festivo FROM Lectivos WHERE Fecha = '$dia' AND Festivo = 'no'")) {
-        
         if ($comprobacion->num_rows > 0) {
-            
-            if ($res = $class->query("SELECT ID_PROFESOR, Fecha FROM Fichar WHERE ID_PROFESOR='$_POST[ID]' AND Fecha='$dia'")) {
-                
+            if ($res = $class->query("SELECT ID_PROFESOR, Fecha FROM Fichar WHERE ID_PROFESOR='$id' AND Fecha='$dia'")) {
                 if ($res->num_rows == 0) {
-                    
                     if ($response = $class->query("INSERT INTO Fichar (ID_PROFESOR, F_entrada, F_Salida, DIA_SEMANA, Fecha)
-                    VALUES ('$_POST[ID]', '$_POST[horaentrada]', '$_POST[horasalida]', '$diasemana', '$dia')")) {
-                        $OK = 'Registro insertado correctamente';
+                    VALUES ('$id', '$horaentrada', '$horasalida', '$diasemana', '$dia')")) {
+                        $MSG = 'Ok-action';
                     } else {
-                        $ERROR = 'El profesor ya ha fichado en la fecha indicada.';
+                        $MSG = 'Error-Insert';
                     }
                 } else {
-                    $ERR_MSG = $class->ERR_ASYSTECO;
+                    $MSG = 'Error-Ya-Fichado';
                 }
             } else {
-                $ERR_MSG = $class->ERR_ASYSTECO;
+                $MSG = 'Error-inesperado';
             }
         } else {
-            $ERROR = 'No se puede fichar en dias festivos.';
+            $MSG = 'Error-Festivo';
         }
     } else {
-        $ERR_MSG = $class->ERR_ASYSTECO;
+        $MSG = 'Error-inesperado';
     }
 } else {
-    $ERROR = 'Error de Fecha';
+    $MSG = 'Error-Formato-Fecha';
 }
+echo $MSG;
