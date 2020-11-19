4ed23b0172225171ef5ff2ebb8c1da85dacff5e6
asysteco <admin@asysteco.com>
2020-11-19 18:38:19 +0100

    Importar Horario con Edificio

--- inc/preview-import-horario.php
+++ inc/preview-import-horario.php
@@ -12,6 +12,7 @@ if ($response->num_rows > 0) {
 } else {
     exit;
 }
+$edificio = 1;
 
 require_once($dirs['class'] . 'ImportHorario.php');
 $fileName = $_FILES["file"]["tmp_name"];
@@ -36,6 +37,14 @@ if ($_FILES["file"]["size"] > 0) {
                 exit;
             }
         }
+        if (isset($column[5])) {
+            if (preg_match('/^[1-9]$/', $column[5])) {
+                $edificio = $column[5];
+            } else {
+                echo "error-edificio";
+                exit;
+            }
+        }
 
         $importHorario = new ImportHorario(utf8_encode($column[0]), utf8_encode($column[1]), utf8_encode($column[2]), $column[3], $column[4]);
 
@@ -56,6 +65,7 @@ if ($_FILES["file"]["size"] > 0) {
         echo "<td>" . $importHorario->aula() . "</td>";
         echo "<td>" . $importHorario->dia() . "</td>";
         echo "<td>" . $importHorario->hora() . "</td>";
+        echo "<td>" . $edificio . "</td>";
         echo '</tr>';
         $row++;
     }
