629a27bf933fc39305fceebac9f1fb5f24681a07
griffo70 <pedro@griffo.es>
2020-12-01 18:27:46 +0100

    Reestructurar carpetas

--- inc/Login/admin-login.php
+++ inc/Login/admin-login.php
@@ -2,7 +2,7 @@
 if(isset($_GET['criptedval']) && $_GET['criptedval'] != 'undefined')
 {
   $_GET['criptedval'] = preg_replace('/\s/', '+', urldecode($_GET['criptedval']));
-  include($dirs['inc'] . 'mcript.php');
+  include($dirs['inc'] . 'Helper/mcript.php');
   $dato_desencriptado = $desencriptar($_GET['criptedval']);
   $_GET['ID'] = $dato_desencriptado;
   if(isset($_GET['ID']) && $_GET['ID'] != 'undefined' && $_GET['ID'] != '')
@@ -11,19 +11,23 @@ if(isset($_GET['criptedval']) && $_GET['criptedval'] != 'undefined')
       {
         if($response->num_rows == 1)
         {
-          $nombre = $response->fetch_assoc();
-          if($class->FicharWeb($options['ficharSalida']))
-          {
-              echo "<span id='okqr' style='color: white; font-weight: bolder; background-color: green;'><h3>Fichaje de asistencia correcto.<br> $nombre[Nombre]</h3></span>";
-          }
-          else
-          {
-              echo $class->ERR_ASYSTECO;
-          }
+            if($class->LoginAdminQR($_GET['ID'], $Titulo))
+            {
+                echo "<span id='okqr' style='color: white; font-weight: bolder; background-color: green;'><h3>Lector activado correctamente.<br> $nombre[Nombre]</h3></span>";
+                echo '
+                <script>
+                    location.href = "index.php?ACTION=guardias";
+                </script>
+                ';
+            }
+            else
+            {
+                echo "<span id='noqr' style='color: white; font-weight: bolder; background-color: red;'><h3>$class->ERR_ASYSTECO</h3></span>";
+            }            
         }
         else
         {
-          echo "<span id='noqr' style='color: white; font-weight: bolder; background-color: red;'><h3>Código QR incorrecto.</h3></span>";
+            echo "<span id='noqr' style='color: white; font-weight: bolder; background-color: red;'><h3>Solo un Administrador puede iniciar el lector.</h3></span>";
         }
       }
       else
