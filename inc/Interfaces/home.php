e16b449c073d8ccd383d5f0fb4620bcd2eef096a
varo45 <alvaromelkunas9@gmail.com>
2020-12-09 17:35:25 +0100

    Añadidas variables de directorios

--- inc/Interfaces/home.php
+++ inc/Interfaces/home.php
@@ -6,10 +6,10 @@ if($_SESSION['Perfil'] === 'Admin')
         echo '<div class="container-fluid" style="margin-top:50px">';
             echo "<div class='row'>";
             echo "<div id='qreader' class='col-xs-12 col-md-4' >";
-                include($dirs['inc'] . 'Qr/qr-reader.php');
+                include($dirs['Qr'] . 'qr-reader.php');
             echo "</div>";
             echo "<div class='col-xs-12 col-md-8' style='text-align: center;'>";
-                include($dirs['inc'] . 'Horarios/guardias.php');
+                include($dirs['Horarios'] . 'guardias.php');
             echo "</div>";
             echo "</div>";
         echo "</div>"; 
@@ -26,10 +26,10 @@ if($_SESSION['Perfil'] === 'Admin')
         echo '<div class="container-fluid" style="margin-top:50px">';
             echo "<div class='row'>";
             echo "<div id='qreader' class='col-xs-12 col-md-4' >";
-                include($dirs['inc'] . 'Qr/qr-webcam.php');
+                include($dirs['Qr'] . 'qr-webcam.php');
             echo "</div>";
             echo "<div class='col-xs-12 col-md-8' style='text-align: center;'>";
-                include($dirs['inc'] . 'Horarios/guardias.php');
+                include($dirs['Horarios'] . 'guardias.php');
             echo "</div>";
             echo "</div>";
         echo "</div>"; 
@@ -46,7 +46,7 @@ else
 echo '<div class="container-fluid" style="margin-top:50px">';
     echo "<div class='row'>";
         echo "<div class='col-xs-12' style='text-align: center;'>";
-            include($dirs['inc'] . 'Horarios/guardias.php');
+            include($dirs['Horarios'] . 'guardias.php');
         echo "</div>";
     echo "</div>";
 echo "</div>";
