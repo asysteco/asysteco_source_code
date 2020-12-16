8026ac1184be3e6e6e9bd5e5e7dfd57ce42ddfcf
Jokerrdl <ruizdeluna15@gmail.com>
2020-12-16 19:58:35 +0100

    refactorizacion_listados_admon

--- inc/Listar/list_asistencias.php
+++ inc/Listar/list_asistencias.php
@@ -1,177 +1,145 @@
 <?php
 
-if(isset($_GET['profesor']) && $_GET['profesor'] != '')
-{
-    $profesor = " AND ID_PROFESOR = '$_GET[profesor]'";
-    $selectprofesor = "&profesor=$_GET[profesor]";
-    $sql = "SELECT Marcajes.*, Nombre, Iniciales, Diasemana.Diasemana
-    FROM (Marcajes INNER JOIN Profesores ON Marcajes.ID_PROFESOR=Profesores.ID)
-        INNER JOIN Diasemana ON Marcajes.Dia=Diasemana.ID 
-    WHERE Asiste=1 OR Asiste=2 AND ID_PROFESOR = '$_GET[profesor]'
-    ORDER BY Profesores.Nombre ASC";
-}
-else
-{
-    $profesor = "";
-    $selectprofesor = "";
-    $sql = "SELECT Marcajes.*, Nombre, Iniciales, Diasemana.Diasemana
-    FROM (Marcajes INNER JOIN Profesores ON Marcajes.ID_PROFESOR=Profesores.ID)
-        INNER JOIN Diasemana ON Marcajes.Dia=Diasemana.ID 
-    WHERE Asiste=1 OR Asiste=2 
-    ORDER BY Profesores.Nombre ASC";
-}
-
+$profesor = $_GET['profesor'] ?? '';
+$fechaInicio = $_GET['fechainicio'] ?? '';
+$fechaFin = $_GET['fechafin'] ?? '';
+$whereFilter = ' AND Fecha <= CURDATE()';
+$errorMessage = '';
+$element = $_GET['element'];
 $offset_var = $_GET['pag'];
-if(isset($_GET['fechainicio']) && isset($_GET['fechafin']))
-{
-    $fi = preg_split('/\//', $_GET['fechainicio']);
-            $dia = $fi[0];
-            $m = $fi[1];
-            $Y = $fi[2];
-    $fini = $Y .'-'. $m .'-'. $dia;
-    $ff = preg_split('/\//', $_GET['fechafin']);
-            $dia = $ff[0];
-            $m = $ff[1];
-            $Y = $ff[2];
-    $ffin = $Y .'-'. $m .'-'. $dia;
-    if($class->validFormSQLDate($fini) && $class->validFormSQLDate($ffin))
-    {
-        $selectfecha = "&fechainicio=$_GET[fechainicio]&fechafin=$_GET[fechafin]";
-        if(! $response = $class->query("SELECT ID_PROFESOR FROM Marcajes INNER JOIN Profesores ON Marcajes.ID_PROFESOR=Profesores.ID WHERE (Asiste=1 OR Asiste=2) AND Fecha BETWEEN '$fini' AND '$ffin'"))
-        {
-            die($class->ERR_ASYSTECO);
-        }
-    }
-    else
-    {
-        $selectfecha = "";
-    }
-}
-else
-{
-    if(! $response = $class->query("SELECT Marcajes.*, Nombre, Iniciales, Diasemana.Diasemana
-    FROM (Marcajes INNER JOIN Profesores ON Marcajes.ID_PROFESOR=Profesores.ID)
-        INNER JOIN Diasemana ON Marcajes.Dia=Diasemana.ID 
-    WHERE Asiste=1 OR Asiste=2 
-    ORDER BY Profesores.Nombre ASC"))
-    {
-        die($class->ERR_ASYSTECO);
-    }
-    $selectfecha = "";
-}
-
-if(isset($_GET['fechainicio']) && isset($_GET['fechafin']) && $_GET['fechainicio'] !='' && $_GET['fechafin'] !='')
-{
-    $fechas=" AND Fecha BETWEEN '$fini' AND '$ffin'";
-}
-else
-{
-    $fechas="";
-}
-
 $page_size = 200;
+
+if (isset($profesor) && !empty($profesor)) {
+    $whereFilter .= " AND ID_PROFESOR = $profesor";
+}
+
+
+if (isset($fechaInicio) && !empty($fechaInicio) && isset($fechaFin) && !empty($fechaFin)) {
+    $fini = $class->formatEuropeanDateToSQLDate($fechaInicio);
+    $ffin = $class->formatEuropeanDateToSQLDate($fechaFin);
+
+    if($fini && $ffin) {
+        $whereFilter .= " AND Fecha >= '$fini' AND Fecha <= '$ffin'";
+    }
+}
+
+$sql = "SELECT Marcajes.*, Nombre, Iniciales, Diasemana.Diasemana  
+FROM (Marcajes INNER JOIN Profesores ON Marcajes.ID_PROFESOR=Profesores.ID) 
+INNER JOIN Diasemana ON Marcajes.Dia=Diasemana.ID 
+WHERE (Asiste=1 OR Asiste=2) $whereFilter
+ORDER BY Marcajes.Fecha, Profesores.Nombre ASC 
+LIMIT $page_size OFFSET $offset_var";
+
+if(! $response = $class->query($sql)) {
+    $errorMessage = 'Ha ocurrido un error inesperado...';
+}
+
 $total_records = $response->num_rows;
 $count=ceil($total_records/$page_size);
 
-if(isset($_GET['pag']))
-{
-    echo "<div class='páginas' style='margin-top: 25px;'>";
-        echo "<h3>Página ";
-        echo "<select id='select_pag'>";
-            for($j=0; $j<$count; $j++)
-            {
-                if($_GET['pag'] == $j*$page_size)
+if (empty($errorMessage) && $response->num_rows > 0) {
+    if(isset($offset_var)) {
+        if($count > 1) {
+        echo "<div class='páginas' style='margin-top: 25px;'>";
+            echo "<h3>Página ";
+            echo "<select id='select_pag'>";
+            for($j=0; $j<$count; $j++) {
+                $currentPage = $j*$page_size;
+                $selected = $offset_var == $j*$page_size ? 'selected' : '';
+                echo "<option value='$currentPage' action='select' element='$element' profesor='$profesor' start='$fechaInicio' end='$fechaFin' $selected>";
+                    echo $pag = ($j+1);
+                echo '</option> ';
+            }
+            echo "</select>";
+            echo "</h3>";
+        echo "</div>";
+        }
+        echo "<table class='table table-striped'>";
+            echo "<thead>";
+                echo "<tr>";
+                    echo "<th>INICIALES</th>";
+                    echo "<th>PROFESOR</th>";
+                    echo "<th>FECHA</th>";
+                    echo "<th>HORA</th>";
+                    echo "<th>DIA</th>";
+                    echo "<th>DIA SEMANA</th>";
+                    echo "<th>ASISTENCIA</th>";
+                    echo "<th>ACTIVIDAD EXTRAESCOLAR</th>";
+                echo "</tr>";
+            echo "</thead>";
+            echo "<tbody>";
+    
+        while ($datos = $response->fetch_assoc())
+        {
+            $fecha = $class->formatSQLDateToEuropeanDate($datos['Fecha']);
+            echo "<tr>";
+                echo "<td>$datos[Iniciales]</td>";
+                echo "<td>$datos[Nombre]</td>";
+                echo "<td>$fecha</td>";
+                echo "<td>$datos[Hora]</td>";
+                echo "<td>$datos[Dia]</td>";
+                echo "<td>$datos[Diasemana]</td>";
+                echo "<td>SI</td>";
+                if($datos['Asiste'] == 2)
                 {
-                    $selected = 'selected';
+                    echo "<td>SI</td>";
                 }
                 else
                 {
-                    $selected = '';
+                    echo "<td>NO</td>";
                 }
-                echo '<option value="index.php?ACTION=admon&OPT=select&select=faltas&pag=' . $j*$page_size . $selectprofesor . $selectfecha . '" class="btn-select" ' . $selected . '><span class="glyphicon glyphicon-eye-open"></span> ' . $pag = ($j+1) . '</option> ';
-            }
-        echo "</select>";
-        echo "</h3>";
-    echo "</div>";
-    if((isset($profesor) && $profesor !='') || (isset($fechas) && $fechas !=''))
-    {
-        $query = "SELECT Marcajes.*, Nombre, Iniciales, Diasemana.Diasemana
-        FROM (Marcajes INNER JOIN Profesores ON Marcajes.ID_PROFESOR=Profesores.ID)
-            INNER JOIN Diasemana ON Marcajes.Dia=Diasemana.ID
-        WHERE (Asiste=1 OR Asiste=2) $profesor $and $fechas
-        ORDER BY Marcajes.Fecha, Profesores.Nombre ASC
-        LIMIT $page_size OFFSET $offset_var";
-    }
-    else
-    {
-        $query = "SELECT Marcajes.*, Nombre, Iniciales, Diasemana.Diasemana
-        FROM (Marcajes INNER JOIN Profesores ON Marcajes.ID_PROFESOR=Profesores.ID)
-            INNER JOIN Diasemana ON Marcajes.Dia=Diasemana.ID 
-        WHERE Asiste=1 OR Asiste=2 
-        ORDER BY Marcajes.Fecha, Profesores.Nombre ASC
-        LIMIT $page_size OFFSET $offset_var";
-    }
-    
-    $result =  $class->query($query);
-    echo "<table class='table table-striped'>";
-        echo "<thead>";
-            echo "<tr>";
-                echo "<th>INICIALES</th>";
-                echo "<th>PROFESOR</th>";
-                echo "<th>FECHA</th>";
-                echo "<th>HORA</th>";
-                echo "<th>DIA</th>";
-                echo "<th>DIA SEMANA</th>";
-                echo "<th>ASISTENCIA</th>";
-                echo "<th>ACTIVIDAD EXTRAESCOLAR</th>";
             echo "</tr>";
-        echo "</thead>";
-        echo "<tbody>";
-
-    while ($datos = $result->fetch_assoc())
-    {
-        $sep = preg_split('/[ -]/', $datos['Fecha']);
-        $dia = $sep[2];
-        $m = $sep[1];
-        $Y = $sep[0];
-        echo "<tr>";
-            echo "<td>$datos[Iniciales]</td>";
-            echo "<td>$datos[Nombre]</td>";
-            echo "<td>$datos[Fecha]</td>";
-            echo "<td>$datos[Hora]</td>";
-            echo "<td>$datos[Dia]</td>";
-            echo "<td>$datos[Diasemana]</td>";
-            echo "<td>SI</td>";
-            if($datos['Asiste'] == 2)
-            {
-                echo "<td>SI</td>";
-            }
-            else
-            {
-                echo "<td>NO</td>";
-            }
-        echo "</tr>";
+        }
+    
+            echo "</tbody>";
+        echo "</table>";
     }
-
-        echo "</tbody>";
-    echo "</table>";
+} else {
+    echo "<h2 style='color: grey;'><i>No existen datos que mostrar.</i></h2>";
 }
-else
-{
-    echo "No hay páginas.<br>";
+if (!empty($errorMessage)) {
+    echo $errorMessage;
 }
 
 echo "<script>";
-    echo "$(document).ready(function () {
-        $('#loading').delay().fadeOut()
-    });";
-    echo "
+echo "$(document).ready(function () {
+    $('#loading').delay().fadeOut()
+});";
+echo "
     $('#select_pag').on('change', function() {
-        $('#btn-response').html(''),
-        $('#loading-msg').html('Cargando...'),
-        $('#loading').show(),
-        enlace = $(this).val(),
-        $('#btn-response').load(enlace)
+        element = $(this).children().attr('element');
+        action = $(this).children().attr('action');
+        page = $(this).val();
+        profesor = $(this).children().attr('profesor');
+        start = $(this).children().attr('start');
+        end = $(this).children().attr('end');
+        urlPath = 'index.php?ACTION=admon&OPT=select';
+        data = {
+            'action': action,
+            'element': element,
+            'profesor': profesor,
+            'fechainicio': start,
+            'fechafin': end,
+            'pag': page
+        };
+        
+        $.ajax({
+            url: urlPath,
+            type: 'GET',
+            data:  data,
+            beforeSend : function() {
+                $('#loading-msg').html('Cargando...');
+                $('#loading').show();
+            },
+            success: function(data) {
+                $('#btn-response').html(data);
+                $('#loading').fadeOut();
+            },
+            error: function(e) {
+                $('#error-modal').modal('show'),
+                $('#error-content-modal').html(e);
+            }          
+        });
     });
-    ";
+";
 echo "</script>";
--- inc/Listar/list_fichaje_fecha.php
+++ inc/Listar/list_fichaje_fecha.php
@@ -1,119 +1,141 @@
 <?php
 
+$fechaInicio = $_GET['fechainicio'] ?? '';
+$fechaFin = $_GET['fechafin'] ?? '';
+$whereFilter = ' WHERE Fecha <= CURDATE()';
+$errorMessage = '';
+$selectfecha = '';
+$element = $_GET['element'];
+
 $offset_var = $_GET['pag'];
-if(isset($_GET['fechainicio']) && isset($_GET['fechafin']))
-{
-    $fi = preg_split('/\//', $_GET['fechainicio']);
-            $dia = $fi[0];
-            $m = $fi[1];
-            $Y = $fi[2];
-    $fini = $Y .'-'. $m .'-'. $dia;
-    $ff = preg_split('/\//', $_GET['fechafin']);
-            $dia = $ff[0];
-            $m = $ff[1];
-            $Y = $ff[2];
-    $ffin = $Y .'-'. $m .'-'. $dia;
-    if($class->validFormSQLDate($fini) && $class->validFormSQLDate($ffin))
-    {
-        if(! $response = $class->query("SELECT ID_PROFESOR FROM Fichar INNER JOIN Profesores ON Fichar.ID_PROFESOR=Profesores.ID AND Fecha BETWEEN '$fini' AND '$ffin'"))
-        {
-            die($class->ERR_ASYSTECO);
-        }
+
+if (isset($fechaInicio) && !empty($fechaInicio) && isset($fechaFin) && !empty($fechaFin)) {
+    $fini = $class->formatEuropeanDateToSQLDate($fechaInicio);
+    $ffin = $class->formatEuropeanDateToSQLDate($fechaFin);
+
+    if($fini && $ffin) {
+        $whereFilter .= " AND Fecha >= '$fini' AND Fecha <= '$ffin'";
+        $selectfecha .= "&fechainicio=$_GET[fechainicio]&fechafin=$_GET[fechafin]";
     }
 }
-else
+if(! $response = $class->query("SELECT ID_PROFESOR FROM Fichar INNER JOIN Profesores ON Fichar.ID_PROFESOR=Profesores.ID"))
 {
-    if(! $response = $class->query("SELECT ID_PROFESOR FROM Fichar INNER JOIN Profesores ON Fichar.ID_PROFESOR=Profesores.ID"))
-    {
-        die($class->ERR_ASYSTECO);
-    }
+    $errorMessage = 'Ha ocurrido un error inesperado...';
 }
+
 
 $page_size = 200;
 $total_records = $response->num_rows;
 $count=ceil($total_records/$page_size);
 
-if(isset($_GET['pag']))
-{
-    echo "<div class='páginas' style='margin-top: 25px;'>";
-        echo "<h3>Página ";
-        echo "<select id='select_pag'>";
-            for($j=0; $j<$count; $j++)
-            {
-                if($_GET['pag'] == $j*$page_size)
-                {
-                    $selected = 'selected';
-                }
-                else
-                {
-                    $selected = '';
-                }
-                echo '<option value="index.php?ACTION=admon&OPT=select&select=fichafe&pag=' . $j*$page_size . '&fechainicio=' . $_GET['fechainicio'] . '&fechafin=' . $_GET['fechafin'] . '" class="btn-select" ' . $selected . '><span class="glyphicon glyphicon-eye-open"></span> ' . $pag = ($j+1) . '</option> ';
+$mysql = $class->conex;
+$mysql->autocommit(FALSE);
+
+if (empty($errorMessage) && $response->num_rows > 0) {
+    try {
+        if(isset($offset_var)) {
+            if ($count > 1) {
+                echo "<div class='páginas' style='margin-top: 25px;'>";
+                    echo "<h3>Página ";
+                    echo "<select id='select_pag'>";
+                    for($j=0; $j<$count; $j++) {
+                        $currentPage = $j*$page_size;
+                        $selected = $offset_var == $j*$page_size ? 'selected' : '';
+                        echo "<option value='$currentPage' action='select' element='$element' start='$fechaInicio' end='$fechaFin' $selected>";
+                            echo $pag = ($j+1);
+                        echo '</option> ';
+                    }
+                    echo "</select>";
+                    echo "</h3>";
+                echo "<div>";
             }
-        echo "</select>";
-        echo "</h3>";
-    echo "<div>";
-    if(isset($_GET['fechainicio']) && isset($_GET['fechafin']) && $_GET['fechainicio'] !='' && $_GET['fechafin'] !='')
-    {
-        $query = "SELECT ID_PROFESOR, Nombre, F_entrada, F_Salida, DIA_SEMANA, Fecha
-        FROM (Fichar INNER JOIN Profesores ON Fichar.ID_PROFESOR=Profesores.ID)
-        WHERE Fecha BETWEEN '$fini' AND '$ffin'
-        ORDER BY Profesores.Nombre ASC
-        LIMIT $page_size OFFSET $offset_var";
-    }
-    else
-    {
-        $query = "SELECT ID_PROFESOR, Nombre, F_entrada, F_Salida, DIA_SEMANA, Fecha
-        FROM (Fichar INNER JOIN Profesores ON Fichar.ID_PROFESOR=Profesores.ID)
-        ORDER BY Profesores.Nombre ASC
-        LIMIT $page_size OFFSET $offset_var";
-    }
-    # "select id from shipment Limit ".$page_size." OFFSET ".$offset_var;
-    $result =  $class->query($query);
-    echo "<table class='table table-striped'>";
-        echo "<thead>";
-            echo "<tr>";
-                echo "<th>NOMBRE</th>";
-                echo "<th>FICHAJE DE ENTRADA</th>";
-                echo "<th>FICHAJE DE SALIDA</th>";
-                echo "<th>DIA SEMANA</th>";
-                echo "<th>FECHA</th>";
-            echo "</tr>";
-        echo "</thead>";
-        echo "<tbody>";
-        while ($datos = $result->fetch_assoc())
-        {
-            $sep = preg_split('/[ -]/', $datos['Fecha']);
-            $dia = $sep[2];
-            $m = $sep[1];
-            $Y = $sep[0];
-            echo "<tr>";
-                echo "<td>$datos[Nombre]</td>";
-                echo "<td>$datos[F_entrada]</td>";
-                echo "<td>$datos[F_Salida]</td>";
-                echo "<td>$datos[DIA_SEMANA]</td>";
-                echo "<td>$dia/$m/$Y</td>";
-            echo "</tr>";
+            $sql = "SELECT ID_PROFESOR, Nombre, F_entrada, F_Salida, DIA_SEMANA, Fecha
+            FROM (Fichar INNER JOIN Profesores ON Fichar.ID_PROFESOR=Profesores.ID)
+            $whereFilter
+            ORDER BY Profesores.Nombre ASC
+            LIMIT $page_size OFFSET $offset_var";
+            if (!$result = $mysql->query($sql)) {
+                throw new Exception('Ha ocurrido un error...');
+            }
+            if($result->num_rows > 0) {
+                echo "<table class='table table-striped'>";
+                    echo "<thead>";
+                        echo "<tr>";
+                            echo "<th>NOMBRE</th>";
+                            echo "<th>FICHAJE DE ENTRADA</th>";
+                            echo "<th>FICHAJE DE SALIDA</th>";
+                            echo "<th>DIA SEMANA</th>";
+                            echo "<th>FECHA</th>";
+                        echo "</tr>";
+                    echo "</thead>";
+                    echo "<tbody>";
+                    while ($datos = $result->fetch_assoc())
+                    {
+                        $sep = preg_split('/[ -]/', $datos['Fecha']);
+                        $dia = $sep[2];
+                        $m = $sep[1];
+                        $Y = $sep[0];
+                        echo "<tr>";
+                            echo "<td>$datos[Nombre]</td>";
+                            echo "<td>$datos[F_entrada]</td>";
+                            echo "<td>$datos[F_Salida]</td>";
+                            echo "<td>$datos[DIA_SEMANA]</td>";
+                            echo "<td>$dia/$m/$Y</td>";
+                        echo "</tr>";
+                    }
+                    echo "</tbody>";
+                echo "</table>";
+            }
         }
-        echo "</tbody>";
-    echo "</table>";
+    } catch (Exception $e) {
+        $errorMessage = $e;
+        $class->conex->rollback();
+    }
+    $class->conex->commit();
+} else {
+    echo "<h2 style='color: grey;'><i>No existen datos que mostrar.</i></h2>";
 }
-else
-{
-    echo "No hay páginas.<br>";
+if (!empty($errorMessage)) {
+    echo $errorMessage;
 }
 
 echo "<script>";
-    echo "$(document).ready(function () {
-        $('#loading').delay().fadeOut()
-    });";
-    echo "
+echo "$(document).ready(function () {
+    $('#loading').delay().fadeOut()
+});";
+echo "
     $('#select_pag').on('change', function() {
-        $('#btn-response').html(''),
-        $('#loading-msg').html('Cargando...'),
-        $('#loading').show(),
-        enlace = $(this).val(),
-        $('#btn-response').load(enlace)
+        element = $(this).children().attr('element');
+        action = $(this).children().attr('action');
+        page = $(this).val();
+        start = $(this).children().attr('start');
+        end = $(this).children().attr('end');
+        urlPath = 'index.php?ACTION=admon&OPT=select';
+        data = {
+            'action': action,
+            'element': element,
+            'fechainicio': start,
+            'fechafin': end,
+            'pag': page
+        };
+        
+        $.ajax({
+            url: urlPath,
+            type: 'GET',
+            data:  data,
+            beforeSend : function() {
+                $('#loading-msg').html('Cargando...');
+                $('#loading').show();
+            },
+            success: function(data) {
+                $('#btn-response').html(data);
+                $('#loading').fadeOut();
+            },
+            error: function(e) {
+                $('#error-modal').modal('show'),
+                $('#error-content-modal').html(e);
+            }          
+        });
     });
-    ";
+";
 echo "</script>";
