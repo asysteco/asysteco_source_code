bcc5fac019c7a1b91bce76c17f34c4faad8b820b
griffo70 <pedro@griffo.es>
2020-12-17 18:54:13 +0100

    Añadido Boton CANCELAR en formulario cambio de contraseña de Admon

--- inc/Login/new_pass.php
+++ inc/Login/new_pass.php
@@ -12,7 +12,8 @@
 					<input type="password" name="act_pass" class="fadeIn second" placeholder="Contraseña Actual" required>
 					<input  minlength="8" type="password" name="new_pass" class="fadeIn third" placeholder="Nueva Contraseña" required>
 					<input minlength="8" type="password" name="new_pass_c" class="fadeIn third" placeholder="Confirmar Nueva Contraseña" required>
-					<input type="submit" name="new_password" value="Confirmar" class="fadeIn fourth">
+          <input type="submit" name="new_password" value="Confirmar" class="fadeIn fourth">
+          <input type="button" style="background-color: #DC3545" name="cancelar_new_password" value="Cancelar" class="fadeIn five" onclick="location.href='index.php?ACTION=profesores'">
 			</form>
     </div>
 </div>
