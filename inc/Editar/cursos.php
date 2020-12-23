7a66f100d966adffda2714f1a34266e5160ac974
varo45 <alvaromelkunas9@gmail.com>
2020-12-23 22:27:25 +0100

    Aulas y cursos style

--- inc/Editar/cursos.php
+++ inc/Editar/cursos.php
@@ -2,8 +2,13 @@
     <div class='row'>
         <div class='col-12'>
             <h1>Gestión de Cursos</h1>
-            <input id="add-curso" type="text" class="form-control" placeholder="Escribe el nombre del nuevo curso..."><br>
-            <a id="add-btn" action="add" class="btn btn-success">Añadir Curso</a><br>
+            <div class="input-group mb-3">
+                <input id="add-curso" type="text" class="form-control" placeholder="Escribe el nombre del nuevo curso..." aria-label="Escribe el nombre del nuevo curso...">
+                <div class="input-group-append">
+                    <a id="add-btn" action="add" class="btn btn-success" type="button">Añadir Curso</a>
+                </div>
+            </div>
+
             <table class="table table-striped">
                 <thead>
                     <tr style="text-align: center;">
@@ -19,9 +24,9 @@
                         while($row = $response->fetch_assoc()){
                             echo "<tr id='fila_$row[ID]' style='text-align: center;'>";
                                 echo "<td style='vertical-align: middle;'>";
-                                    echo "<input id='input_$row[ID]' type='text' class='form-control hide-it' style='width: 30%; display: inline-block; vertical-align: middle;'>";
-                                    echo "<span id='txt_$row[ID]' class='show-it'>$row[Nombre]</span> ";
-                                    echo "<a id='btn_$row[ID]' data='$row[ID]' class='btn btn-success hide-it update' style='vertical-align: middle;'>Aplicar</a>";
+                                    echo "<span id='txt_$row[ID]' class='show-it'>$row[Nombre]</span>";
+                                    echo "<input id='input_$row[ID]' type='text' class='form-control hide-it' style='width: 30%; display: inline-block; vertical-align: middle;'> ";
+                                    echo "<a id='btn_$row[ID]' data='$row[ID]' class='btn btn-success hide-it update' style='vertical-align: middle;'><i class='fa fa-check' aria-hidden='true'></i></a>";
                                 echo "</td>";
                                 echo "<td style='font-size: 25px; vertical-align: middle;'><span fields='edit_$row[ID]' class='fa fa-pencil-square-o edit'></span></td>";
                                 echo "<td style='font-size: 25px; vertical-align: middle;'><span data='$row[ID]' action='remove' class='fa fa-trash-o remove'></span></td>";
