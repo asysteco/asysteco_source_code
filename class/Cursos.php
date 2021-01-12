2c87e65dbafe62f0fe337b89999fb58de80d93f6
varo45 <alvaromelkunas9@gmail.com>
2021-01-12 18:43:34 +0100

    Validación con tildes en nombres, fichar fuera horario

--- class/Cursos.php
+++ class/Cursos.php
@@ -4,7 +4,7 @@ class Cursos
 {
     public function cursoValido($curso)
     {
-        if (isset($curso) && preg_match('/^[a-zA-Z0-9 -]{2,25}$/i', $curso)) {
+        if (isset($curso) && preg_match('/^[a-zA-ZäÄëËïÏöÖüÜáéíóúáéíóúÁÉÍÓÚÂÊÎÔÛâêîôûàèìòùÀÈÌÒÙñÑ0-9 -]{2,25}$/i', $curso)) {
             return true;
         }
 
