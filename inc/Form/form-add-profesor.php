9efd23267c52bbfea6cc33d508f4d6d8913d8266
varo45 <alvaromelkunas9@gmail.com>
2021-01-14 21:52:27 +0100

    Deleted Warnings

--- inc/Form/form-add-profesor.php
+++ inc/Form/form-add-profesor.php
@@ -1,18 +1,23 @@
 <?php
-echo '<div class="container">';
-    echo '<div class="wrapper fadeInDown">';
-        echo '<div id="formContent">';
-            echo "<h1>Registrar Profesor</h1>";
-            echo "<form action='index.php?ACTION=profesores&OPT=add-profesor' method='post'>";
-            echo "<input type='text' name='Iniciales' value='$_POST[Iniciales]' class='form-control' placeholder='Iniciales Profesor'>";
-            echo "</br>";
-            echo "<input type='text' name='Nombre' value='$_POST[Nombre]' class='form-control' placeholder='Nombre Profesor (Completo)'>";
-            echo "</br>";
-            echo "<div style='display: inline'>";
-            echo "<button class='btn btn-success float-right' style='margin-right: 33px; margin-bottom: 10px; margin-top: 15px' value='add' name='add-profesor'>Registrar</button>";
-            echo "<a href='index.php?ACTION=profesores' class='btn btn-danger float-left' style='margin-left: 33px; margin-top: 15px'>Cancelar</a>";
-            echo "</div>";
-            echo "</form>";
-        echo "</div>";
-    echo "</div>";
-echo "</div>";
+
+    $iniciales = $_POST['Iniciales'] ?? '';
+    $nombre = $_POST['Nombre'] ?? '';
+
+?>
+<div class="container">
+    <div class="wrapper fadeInDown">
+        <div id="formContent">
+            <h1>Registrar Profesor</h1>
+            <form action='index.php?ACTION=profesores&OPT=add-profesor' method='post'>
+            <input type='text' name='Iniciales' value='<?=$iniciales?>' class='form-control' placeholder='Iniciales Profesor'>
+            </br>
+            <input type='text' name='Nombre' value='<?=$nombre?>' class='form-control' placeholder='Nombre Profesor (Completo)'>
+            </br>
+            <div style='display: inline'>
+            <button class='btn btn-success float-right' style='margin-right: 33px; margin-bottom: 10px; margin-top: 15px' value='add' name='add-profesor'>Registrar</button>
+            <a href='index.php?ACTION=profesores' class='btn btn-danger float-left' style='margin-left: 33px; margin-top: 15px'>Cancelar</a>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
