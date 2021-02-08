dbd544c25b6a282a4b5a4ff0f33f3e98df4e366c
varo45 <alvaromelkunas9@gmail.com>
2021-02-08 09:53:01 +0100

    Arreglo imports

--- inc/CP/cp-header.php
+++ inc/CP/cp-header.php
@@ -4,12 +4,12 @@
     <title>Activar Lector</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <link rel="stylesheet" href="css/bootstrap-3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-4.0.0/bootstrap.min.css">
     <link rel="stylesheet" href="css/asysteco.css">
     <link rel="stylesheet" href="js/jquery-ui/jquery-ui.min.css">
     <link rel="shortcut icon" href="resources/img/asysteco.ico" type="image/x-icon">
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery/jquery.min.js"></script>
+    <script src="js/bootstrap-4.0.0/bootstrap.min.js"></script>
     <script src="js/jquery-ui/jquery-ui.min.js"></script>
     <script src="js/datepicker_common.js"></script>
     <script src="js/flecha.js"></script>
