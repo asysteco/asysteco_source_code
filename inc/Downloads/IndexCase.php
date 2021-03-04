604445448f860185216f2608c3d1b8a872dc83bb
varo45 <alvaromelkunas9@gmail.com>
2021-03-04 20:46:33 +0100

    Fichar Manual y Downloads Ok

--- inc/Downloads/IndexCase.php
+++ inc/Downloads/IndexCase.php
@@ -1,35 +1,31 @@
 <?php
 
-if (!isset($_GET['OPT'])) {
-    $_GET['OPT'] = '';
-}
+$opt = $_GET['OPT'] ?? '';
 
 switch ($_GET['OPT']) {
     case 'plantilla-horarios':
         if ($_SESSION['Perfil'] === 'Admin') {
-        require_once($dirs['Downloads'] . 'plantilla-horarios.php');
+            require_once($dirs['Downloads'] . 'plantilla-horarios.php');
         }
-    break;
+        break;
 
     case 'plantilla-profesores':
         if ($_SESSION['Perfil'] === 'Admin') {
-        require_once($dirs['Downloads'] . 'plantilla-profesores.php');
+            require_once($dirs['Downloads'] . 'plantilla-profesores.php');
         }
-    break;
+        break;
 
     case 'admin-guide':
         if ($_SESSION['Perfil'] === 'Admin') {
-        require_once($dirs['Downloads'] . 'guide-admin.php');
+            require_once($dirs['Downloads'] . 'guide-admin.php');
         }
-    break;
+        break;
 
     case 'profesor-guide':
         require_once($dirs['Downloads'] . 'guide-profesor.php');
-    break;
+        break;
 
-    default:    
-        $MSG = "Acción errónea.";
-        header("Refresh:2; url=index.php");
-        include_once($dirs['Interfaces'] . 'msg_modal.php');
-    break;
+    default:
+        header("location: index.php");
+        break;
 }
